d71380af0fe5fc1c97f2b415af9eb711c3554725
AlexAlbarran <alejandro.albarran.01@gmail.com>
2024-03-13 20:17:49 -0500

    first commit

---
+++ src/main/java/org/example/helloworld.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello Mars");
+    }
+}
